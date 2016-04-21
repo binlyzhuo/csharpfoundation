8a171174e867b26ca80e505b8e4e45945e0dde69
binlyzhuo <binlyzhuo@outlook.com>
2016-04-21 15:52:25 +0800

    ~commit

---
+++ CSharpFoundation/ThreadHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CSharpFoundation
+{
+    public class ThreadHelper
+    {
+        static void GetThreadInfo()
+        {
+            Thread thread = new Thread(Work);
+            thread.IsBackground = true;
+
+            ThreadPool.QueueUserWorkItem(Work, "");
+        }
+
+        static void Work(object stat)
+        {
+            Console.WriteLine("I am working");
+        }
+    }
+}
