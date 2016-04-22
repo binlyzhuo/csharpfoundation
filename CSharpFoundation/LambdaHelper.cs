83cc500bb8c4cc4f17e51e37b6afdbbcc162a3f2
binlyzhuo <binlyzhuo@outlook.com>
2016-04-22 14:48:58 +0800

    ~commit codes

---
+++ CSharpFoundation/LambdaHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFoundation
+{
+    public class LambdaHelper
+    {
+        private delegate int del(int i);
+        public static void ExecuteCodes()
+        {
+            del myDel = x => x*x;
+            int j = myDel(5);
+        }
+    }
+
+    public class People
+    {
+        public int Age { set; get; }
+        public string Name { set; get; }
+
+        public People(int age, string name)
+        {
+            this.Age = age;
+            this.Name = name;
+        }
+    }
+}
