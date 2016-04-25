a67652a89506ec91333692d0adbfbbec8a6dde4c
binlyzhuo <binlyzhuo@outlook.com>
2016-04-25 13:54:09 +0800

    ~commit

---
+++ CSharpFoundation/AOPHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFoundation
+{
+    public class AOPHelper
+    {
+        public class Order
+        {
+            public int Id { set; get; }
+            public string Name { set; get; }
+            public int Count { set; get; }
+        }
+    }
+}
